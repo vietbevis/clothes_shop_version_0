48506003fd50ce586395bbb63786c486cf4f6006
Nguyễn Văn Việt <nguyenvanviet.150204@gmail.com>
2025-01-18 17:48:19 +0700

    ok

--- src/config/corsConfig.ts
+++ src/config/corsConfig.ts
@@ -1,9 +1,21 @@
 import { type CorsOptions } from 'cors'
 
+const allowedOrigins = [
+  'http://localhost:3000',
+  'http://localhost:3001',
+  'http://localhost:4000',
+  'http://localhost:5173'
+]
+
 export const corsConfig: CorsOptions = {
   optionsSuccessStatus: 200,
   credentials: true,
   origin: function (origin, callback) {
-    return callback(null, true)
+    // Kiểm tra nếu origin nằm trong danh sách cho phép hoặc là undefined (trường hợp non-browser client)
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true) // Cho phép truy cập
+    } else {
+      callback(new Error('Not allowed by CORS')) // Từ chối truy cập
+    }
   }
 }
